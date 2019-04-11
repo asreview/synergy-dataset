57f21e0fd22e9bdf77ac46adb4d97a0210a53579
Raoul Schram <r.d.schram@uu.nl>
2019-04-11 14:30:15 +0200

    Modified script to output readable csv files.

--- datasets/Cohen_EBM/clean_cohen.py
+++ datasets/Cohen_EBM/clean_cohen.py
@@ -1,33 +1,55 @@
+#!/usr/bin/env python
+
 import os
+import sys
 
 import pandas as pd
 
 
-def load_drug_data(name):
-    """Load drug datasets and their labels.
+def name_from_fp(fp):
+    base_name = os.path.basename(fp)
+    name, _ = os.path.splitext(base_name)
+    return name
 
-    params
-    ------
-    name: str
-        The name of the dataset (should match with file name)
+
+def clean_drug_data(fp_in, fp_out):
+    """Load drug datasets, add a new column "included_final" and write to a file.
+
+    Aruments
+    --------
+    fp_in: str
+        File path name to read csv file from.
+    fp_out: str
+        File path name to write csv file to.
     """
 
-    print("load drug dataset: {}".format(name))
-
-    # create file path based on the argument name.
-    fp = os.path.join(DRUG_DIR, name + ".csv")
+    name = name_from_fp(fp_in)
 
     try:
-        df = pd.read_csv(fp)
+        df = pd.read_csv(fp_in)
     except FileNotFoundError:
-        raise ValueError("Dataset with name {} doesn't exist".format(name))
+        msg = "Dataset with name {} doesn't exist".format(name)
+        raise FileNotFoundError(msg)
 
-    # make texts and labels
-    texts = (df['title'].fillna('') + ' ' + df['abstracts'].fillna(''))
-    labels = (df["label2"] == "I").astype(int)
+    df = df.assign(included_final=lambda x: (x.label2 == "I").astype(int))
 
-    print("number of positive labels: {}".format(labels.sum()))
-    print("relative number of positive labels: {}".format(
-        labels.sum() / labels.shape[0]))
+    n_included = df['included_final'].sum()
+    n_total = len(df['included_final'])
+    perc_included = round(100*n_included/float(n_total), 2)
+    msg = """\
+*******************************************
+**   Dataset  : {name: <25}**
+** # Samples  : {n_total: <25}**
+** # Included : {n_included: <25}**
+** % Included : {perc_included: <25}**
+*******************************************
+"""
+    print(msg.format(name=name, n_total=n_total, n_included=n_included,
+                     perc_included=perc_included))
+    df.to_csv(fp_out)
 
-    return texts.values, labels.values
+
+if __name__ == "__main__":
+    fp_in = sys.argv[1]
+    fp_out = sys.argv[2]
+    clean_drug_data(fp_in, fp_out)
