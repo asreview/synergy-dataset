5e6b2057f6b51f0c69e3ef2a3ca919a407a6966b
Raoul Schram <r.d.schram@uu.nl>
2019-04-17 16:29:31 +0200

    Add requirements + update README + set default dir

--- datasets/Cohen_EBM/clean_cohen.py
+++ datasets/Cohen_EBM/clean_cohen.py
@@ -6,7 +6,7 @@ import sys
 import pandas as pd
 
 
-def name_from_fp(fp):
+def _name_from_fp(fp):
     base_name = os.path.basename(fp)
     name, _ = os.path.splitext(base_name)
     return name
@@ -23,18 +23,19 @@ def clean_drug_data(fp_in, fp_out):
         File path name to write csv file to.
     """
 
-    name = name_from_fp(fp_in)
+    name = _name_from_fp(fp_in)
 
     try:
         df = pd.read_csv(fp_in)
     except FileNotFoundError:
         msg = "Dataset with name {} doesn't exist".format(name)
-        raise FileNotFoundError(msg)
+        print(msg)
+        sys.exit(192)
 
-    df = df.assign(included_final=lambda x: (x.label2 == "I").astype(int))
-
-    n_included = df['included_final'].sum()
-    n_total = len(df['included_final'])
+    df = df.assign(label_included=lambda x: (x.label2 == "I").astype(int))
+    df.rename(columns={'abstracts': 'abstract'}, inplace=True)
+    n_included = df['label_included'].sum()
+    n_total = len(df['label_included'])
     perc_included = round(100*n_included/float(n_total), 2)
     msg = """\
 *******************************************
@@ -50,6 +51,16 @@ def clean_drug_data(fp_in, fp_out):
 
 
 if __name__ == "__main__":
+    num_arg = len(sys.argv)-1
+    if num_arg <= 0:
+        print("Error: supply file name.")
+        sys.exit(192)
+    elif num_arg == 1:
+        dir_name = "clean_cohen_review"
+        filename = "{name}.csv".format(name=_name_from_fp(sys.argv[1]))
+        os.makedirs(dir_name, exist_ok=True)
+        fp_out = os.path.join(dir_name, filename)
+    else:
+        fp_out = sys.argv[2]
     fp_in = sys.argv[1]
-    fp_out = sys.argv[2]
     clean_drug_data(fp_in, fp_out)
