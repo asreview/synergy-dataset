304d2486f70e6040d7e9fb05711231cd853f0a80
qubixes <44498096+qubixes@users.noreply.github.com>
2019-04-24 15:28:57 +0200

    Various improvements to collect Cohen datasets

--- datasets/Cohen_EBM/README.md
+++ datasets/Cohen_EBM/README.md
@@ -1,5 +1,8 @@
 # Evidence-based medicine
 
+This is a standard dataset from the medical sciences to test the performance of automated review systems such as the ASR project. This readme describes the process to obtain CSV data files that can be used by the ASR software to benchmark/test its performance.
+
+
 **Reducing Workload in Systematic Review Preparation Using Automated Citation Classification**
 A.M. Cohen, MD, MS, W.R. Hersh, MD, K. Peterson, MS, and Po-Yin Yen, MS
 https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1447545/
@@ -9,3 +12,26 @@ https://www.ncbi.nlm.nih.gov/pmc/articles/PMC1447545/
 ``` sh
 Rscript pubmed_retrieval.R
 ```
+
+## Process
+
+
+### Individual files:
+
+If we want to clean some file `bash $SRC_FILE` into a file `bash $DST_FILE`:
+
+``` bash
+./clean_cohen.py $SRC_FILE $DST_FILE
+```
+
+### Complete directories:
+
+If we have a directory `bash $SRC_DIR` with csv files:
+
+``` bash
+./clean_cohen_dir.sh $SRC_DIR $DST_DIR
+```
+
+## Copy
+
+You will probably want to copy the processed files to some working directory.
--- datasets/Cohen_EBM/clean_cohen.py
+++ datasets/Cohen_EBM/clean_cohen.py
@@ -1,33 +1,66 @@
+#!/usr/bin/env python
+
 import os
+import sys
 
 import pandas as pd
 
 
-def load_drug_data(name):
-    """Load drug datasets and their labels.
+def _name_from_fp(fp):
+    base_name = os.path.basename(fp)
+    name, _ = os.path.splitext(base_name)
+    return name
 
-    params
-    ------
-    name: str
-        The name of the dataset (should match with file name)
+
+def clean_drug_data(fp_in, fp_out):
+    """Load drug datasets, add a new column "included_final" and write to a file.
+
+    Aruments
+    --------
+    fp_in: str
+        File path name to read csv file from.
+    fp_out: str
+        File path name to write csv file to.
     """
 
-    print("load drug dataset: {}".format(name))
-
-    # create file path based on the argument name.
-    fp = os.path.join(DRUG_DIR, name + ".csv")
+    name = _name_from_fp(fp_in)
 
     try:
-        df = pd.read_csv(fp)
+        df = pd.read_csv(fp_in)
     except FileNotFoundError:
-        raise ValueError("Dataset with name {} doesn't exist".format(name))
+        msg = "Dataset with name {} doesn't exist".format(name)
+        print(msg)
+        sys.exit(192)
 
-    # make texts and labels
-    texts = (df['title'].fillna('') + ' ' + df['abstracts'].fillna(''))
-    labels = (df["label2"] == "I").astype(int)
+    df = df.assign(label_included=lambda x: (x.label2 == "I").astype(int))
+    df.rename(columns={'abstracts': 'abstract'}, inplace=True)
+    n_included = df['label_included'].sum()
+    n_total = len(df['label_included'])
+    perc_included = round(100*n_included/float(n_total), 2)
+    msg = """\
+*******************************************
+**   Dataset  : {name: <25}**
+** # Samples  : {n_total: <25}**
+** # Included : {n_included: <25}**
+** % Included : {perc_included: <25}**
+*******************************************
+"""
+    print(msg.format(name=name, n_total=n_total, n_included=n_included,
+                     perc_included=perc_included))
+    df.to_csv(fp_out)
 
-    print("number of positive labels: {}".format(labels.sum()))
-    print("relative number of positive labels: {}".format(
-        labels.sum() / labels.shape[0]))
 
-    return texts.values, labels.values
+if __name__ == "__main__":
+    num_arg = len(sys.argv)-1
+    if num_arg <= 0:
+        print("Error: supply file name.")
+        sys.exit(192)
+    elif num_arg == 1:
+        dir_name = "clean_cohen_review"
+        filename = "{name}.csv".format(name=_name_from_fp(sys.argv[1]))
+        os.makedirs(dir_name, exist_ok=True)
+        fp_out = os.path.join(dir_name, filename)
+    else:
+        fp_out = sys.argv[2]
+    fp_in = sys.argv[1]
+    clean_drug_data(fp_in, fp_out)
