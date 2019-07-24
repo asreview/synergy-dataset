3ee138dda20ade3d964d035f1cd554f733ddbdb2
Raoul Schram <r.d.schram@uu.nl>
2019-07-24 17:35:44 +0200

    Add the two missing papers from AAS
    Also set AAS to 1 if it is finally included.

--- datasets/Van_de_Schoot_PTSD/clean_schoot_lgmm_ptsd.R
+++ datasets/Van_de_Schoot_PTSD/clean_schoot_lgmm_ptsd.R
@@ -1,21 +1,14 @@
-require(tidyverse)
+suppressMessages(require(tidyverse))
 
 #original papers
 setwd("csv")
-all.data <- read_csv("schoot-lgmm-ptsd-initial.csv")
-
-sum(!is.na(all.data$abstract) & !is.na(all.data$title))
-
+all.data <- read_csv("schoot-lgmm-ptsd-initial.csv", col_types=cols())
 
 #included papers
-inc.data <- read_csv("schoot-lgmm-ptsd-included-2.csv")
+inc.data <- read_csv("schoot-lgmm-ptsd-included-2.csv", col_types=cols())
 
 #After abstract screening
-aas.data <- read_csv("schoot-lgmm-ptsd-included-1.csv")
-
-
-# inc.data$title[! inc.data$title %in% all.data$title]
-
+aas.data <- read_csv("schoot-lgmm-ptsd-included-1.csv", col_types=cols())
 
 #all titles (clean)
 all.title <- gsub("[^A-Za-z0-9]","", all.data$title)
@@ -24,17 +17,23 @@ inc.title <- gsub("[^A-Za-z0-9]","", inc.data$title)
 #After abstract screening title
 aas.title <- gsub("[^A-Za-z0-9]","", aas.data$title)
 
-cat("\nPapers included, missing from initial data:            ", sum(! tolower(inc.title) %in% tolower(all.title)), "\n")
+inc_missing = ! tolower(inc.title) %in% tolower(all.title)
+aas_missing = ! tolower(aas.title) %in% tolower(all.title)
+
+cat("Papers included, missing from initial data:              ", sum(! tolower(inc.title) %in% tolower(all.title)), "\n")
 cat("Papers in abstract screening, missing from initial data: ", sum(! tolower(aas.title) %in% tolower(all.title)), "\n\n")
 
+# Add missing papers to original dataset.
+all.data <- dplyr::mutate(all.data, year=as.character(year))
+all.data <- bind_rows(all.data, aas.data[aas_missing,])
+all.data <- bind_rows(all.data, inc.data[inc_missing,])
+
+# Update "cleaned" titles with new additions.
+all.title <- gsub("[^A-Za-z0-9]","", all.data$title)
 
 #train data
 label_inc <- as.numeric(tolower(all.title) %in% tolower(inc.title))
 label_aas <- as.numeric(tolower(all.title) %in% tolower(aas.title))
-
-# print(label_aas)
-# print(label_inc)
-# label_aas
 
 train.data <- add_column(all.data[, c("authors", "title", "keywords", "abstract")], included = label_inc, aas_included = label_aas) %>%
   mutate(authors = gsub("[\\[']", "", authors),
@@ -42,10 +41,10 @@ train.data <- add_column(all.data[, c("authors", "title", "keywords", "abstract"
 
 
 #64 with missing title
-cat("Number of papers with missing title:             ", sum(is.na(all.title)), "\n")
+cat("Number of papers with missing title:              ", sum(is.na(all.title)), "\n")
 
 #762 with missing abstract
-cat("Number of papers with missing abstract:          ", sum(is.na(all.data$abstract)), "\n")
+cat("Number of papers with missing abstract:           ", sum(is.na(all.data$abstract)), "\n")
 
 #62 with both missing titles and abstracts
 cat("Number of papers with missing title AND abstract: ", sum((is.na(all.title) & is.na(all.data$abstract))), "\n")
@@ -64,12 +63,15 @@ cat("Number of papers with missing title OR abstract:  ", sum((is.na(all.title) 
 unique.position <- duplicated(tolower(all.title), incomparables = NA)
 unique.train.data <- train.data[!unique.position, ]
 
+# Set aas_included to 1 if they are in the final inclusion.
+paper_direct = unique.train.data$aas_included == 0 & unique.train.data$included == 1
+unique.train.data[paper_direct, "aas_included"] <- 1
+
 
 n_train = length(unique.train.data$included)
 n_inc = sum(unique.train.data$included)
 n_aas = sum(unique.train.data$aas_included | unique.train.data$included)
-paper_direct = unique.train.data$aas_included == 0 & unique.train.data$included == 1
-# print(unique.train.data$title[paper_direct])
+
 n_direct = sum(paper_direct)
 cat("Total remaining papers in training set: ", n_train, "\n")
 cat("Papers after abstract screening:        ", n_aas, "\n")
