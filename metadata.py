3ef33689b189e770a20c6b1857ddde8cd32eb2d9
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-12-30 11:08:22 +0100

    Fix encoding

--- metadata.py
+++ metadata.py
@@ -6,9 +6,19 @@ metadata_files = glob.glob("datasets/*/*.json")
 
 metadata = {}
 for dataset_fp in metadata_files:
-	with open(dataset_fp, "r") as f:
-		res = json.load(f)
-		metadata[res["dataset_id"]] = res
+    with open(dataset_fp, "r", encoding="utf-8") as f:
+        res = json.load(f)
 
-with open("index.json", "w") as f_write:
-	json.dump(metadata, f_write, indent=2)
+        # new variables
+        res["type"] = "base"
+
+        # store results
+        metadata[res["dataset_id"]] = res
+
+with open("index.json", "w", encoding="utf-8") as f_write:
+    json.dump(metadata, f_write, indent=2)
+
+
+# # validate
+# with open("index.json", "r") as f_read:
+#     print(json.load(f_read))
