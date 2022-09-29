87cd3e10af0ed016783a33e6bd7a3a2b89cd2bc5
Jonathan de Bruin <jonathandebruinos@gmail.com>
2022-09-29 17:03:58 +0200

    Apply new format

--- datasets/Cohen_2006/etl_doi.py
+++ datasets/Cohen_2006/etl_doi.py
@@ -2,30 +2,14 @@ import pandas as pd
 
 import requests
 
-# file_location = "https://dmice.ohsu.edu/cohenaa/epc-ir-data/epc-ir.clean.tsv"
-file_location = "epc-ir.clean.tsv"
+file_location = "https://dmice.ohsu.edu/cohenaa/epc-ir-data/epc-ir.clean.tsv"
 
-df = pd.read_table(file_location, names=["disease", "endnoteID", "pubmedID", "abstractLabel", "articleLabel"]).head(10)
-df["pubmedID"] = df["pubmedID"].astype(str)
+df = pd.read_table(file_location, names=["disease", "endnoteID", "id", "abstractLabel", "articleLabel"])
+df["id_type"] = "pmid"
 df["label_included"] = (df["articleLabel"] == "I").astype(int)
-
-def pmid_doi(pmid):
-
-	api = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id={pmid}&retmode=json"
-
-	try:
-		r = requests.get(api)
-		print(r.url, r.status_code)
-		return filter(lambda d: d['idtype'] == "doi", r.json()['result'][str(pmid)]['articleids']).__next__()["value"]
-	except Exception as err:
-		return None
-
-# get the DOIs
-df["id"] = df["pubmedID"].map(pmid_doi)
-df["id_type"] = "doi"
 
 # save results to file
 for disease in df["disease"].unique():
-	df[df["disease"] == disease][["id", "id_type", "label_included"]].to_csv(f"{disease}.csv", index=False)
+	df[df["disease"] == disease][["id", "id_type", "label_included"]].to_csv(f"Cohen_2006_{disease}_ids.csv", index=False)
 
 
