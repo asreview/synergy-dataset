adbd1d74f9a44d3a268e2348eec0d1356deb2bb4
JT <j.j.teijema@uu.nl>
2022-11-02 12:55:51 +0100

    Create etl_doi.py

---
+++ datasets/Hamilton_2022/etl_doi.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import re
+
+# set file location
+file_location = "https://osf.io/download/4swnr/"
+
+# identify the used columns
+usecols=["title", "abstract", "issn", "notes"]
+
+# load the data from osf
+df = pd.read_csv(file_location, usecols=usecols)
+
+# notes to label
+regex = r'(?<=RAYYAN-INCLUSION: {)[a-zA-Z"=>, .]+'
+df["full_labels"] = [re.search(regex, note).group() for note in df.notes]
+df["labels"] = [1 if "Included" in note else 0 for note in df.full_labels]
+
+# save results to file
+df[["title", "abstract", "labels"]].to_csv("Hamilton_2022.csv", index=False)
