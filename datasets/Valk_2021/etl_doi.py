bc9959cffb8e0caef683059533153caa0030f6c3
Gijs Mourits <gijsmourits1998@hotmail.com>
2022-11-02 11:22:42 +0100

    cleaner code

--- datasets/Valk_2021/etl_doi.py
+++ datasets/Valk_2021/etl_doi.py
@@ -1,29 +1,16 @@
-import re
-from asreview import ASReviewData
+import pandas as pd
 
+df = pd.read_excel("https://osf.io/download/gmjcv/", usecols=['DOI', 'Included_fulltext'])
 
-dataset = "https://osf.io/download/gmjcv/"
+# adjust columns
+df["DOI"] = df["DOI"].str.extract(r"(10.\S+)")
+df['id_type'] = 'doi'
 
-asr_dataset = ASReviewData.from_file(dataset)
+# rename columns
+df = df.rename({'Included_fulltext': 'label_included', 'DOI': 'id'}, axis=1)
 
-n_records = len(asr_dataset.df)
+# drop missing ids
+df = df.dropna(subset=["id"])
 
-asr_dataset.df['id_type'] = 'doi'
-asr_dataset.df['label_included'] = asr_dataset.df['Included_fulltext']
-asr_dataset.df = asr_dataset.df[asr_dataset.df['DOI'].notna()]
-asr_dataset.df['id'] = asr_dataset.df['DOI']
-
-# Percentage PIDs before deduplication
-print("Percentage of records with PID specified: ", len(asr_dataset.df)/n_records)
-
-# Clean DOIs
-for index, row in asr_dataset.df.iterrows():
-    # Match '10.' followed by at least 1 non whitespace characters, till first whitespace
-    p = re.compile("(10.\S+)")
-    result = p.search(row['id'])
-    asr_dataset.df.at[index, 'id'] = result.group(1)
-
-asr_dataset.df = asr_dataset.df.reindex(columns=['id', 'id_type', 'label_included'])
-
-# save results to file
-asr_dataset.df.to_csv("Valk_2021_ids.csv", index=False)
+# export
+df.to_csv("Valk_2021_ids.csv", columns=['id', 'id_type', 'label_included'], index=False)
