c660847636a810774b47b6e33d3cec12327876f5
Gijs Mourits <gijsmourits1998@hotmail.com>
2022-11-02 12:08:20 +0100

    Cleaner code

--- datasets/Donners_2021/etl_doi.py
+++ datasets/Donners_2021/etl_doi.py
@@ -1,46 +1,22 @@
-import re
-import numpy as np
 import pandas as pd
 from asreview import ASReviewData
 
+# load RIS from OSF into ASReviewData object
+asr_inclusions = ASReviewData.from_file("https://osf.io/download/j8stn/")
+asr_search = ASReviewData.from_file("https://osf.io/download/zf95a/")
 
-file_search = "https://osf.io/download/zf95a/"
-file_inclusions = "https://osf.io/download/j8stn/"
-
-asr_search = ASReviewData.from_file(file_search)
-asr_inclusions = ASReviewData.from_file(file_inclusions)
-
-n_records = len(asr_search.df)
-
-asr_search.df['id_type'] = "doi"
+# set labels and turn into single dataframe
+asr_inclusions.df['label_included'] = 1
 asr_search.df['label_included'] = 0
+df = pd.concat([asr_inclusions.df, asr_search.df], ignore_index=True)
 
-asr_search.df = asr_search.df[asr_search.df['doi'].notna()]
-
-# Percentage PIDs before deduplication
-print("Percentage of records with PID specified: ", len(asr_search.df)/n_records)
-
-dict_doi = asr_search.df.set_index('doi')['label_included'].to_dict()
-
-# Set label_included to 1 for inclusions
-for inclusion_doi in asr_inclusions.df['doi']:
-    if inclusion_doi is not np.nan:
-        dict_doi[inclusion_doi] = 1
-
-# Convert dict to dataframe and reorder columns
-df_output = pd.DataFrame.from_dict(dict_doi, orient='index', columns=['label_included'])
-ids = df_output.index.to_list()
-df_output = df_output.reset_index()
-df_output['id_type'] = "doi"
-df_output['id'] = ids
-df_output = df_output.reindex(columns=['id', 'id_type', 'label_included'])
-
-# Clean DOIs
-for index, row in df_output.iterrows():
-    # Match '10.' followed by atleast 1 non whitespace characters, till first whitespace
-    p = re.compile("(10.\S+)")
-    result = p.search(row['id'])
-    df_output.at[index, 'id'] = result.group(1)
+# adjust columns and drop missing and duplicate ids
+df['doi'] = df['doi'].str.extract(r"(10.\S+)")
+df['id_type'] = 'doi'
+df = df.dropna(subset=['doi'])
+df = df.drop_duplicates(subset=['doi'])
+df['id'] = df['doi']
 
 # save results to file
-df_output.to_csv("Donners_2021_ids.csv", index=False)
+df.to_csv("Donners_2021_ids.csv", columns=['id', 'id_type', 'label_included'], index=False)
+
