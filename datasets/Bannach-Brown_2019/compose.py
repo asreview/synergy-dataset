d59c6359f70cbf964a6104f6bedf155573b37610
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-01-23 22:55:03 +0100

    Add Bannach dataset

---
+++ datasets/Bannach-Brown_2019/compose.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+key = "Bannach-Brown_2019"
+
+url = "https://zenodo.org/record/151190/files/Depression-Dataset-SLIM-DevelopmentTrainingSet.txt"
+df = pd.read_table(url, index_col="ID")
+
+# rename columns
+df.rename(columns={"Incl(1)/Excl(0)":"label_included", "Author":"authors"}, inplace=True)
+df.columns = map(str.lower, df.columns)
+
+# adjust columns
+df["doi"] = None
+
+# export
+df.to_csv(f"{key}_raw.csv", index=False)
+
+df_new = df[["doi", "label_included"]].copy()
+df_new["openalex_id"] = None
+
+df_new[["doi", "openalex_id", "label_included"]].to_csv(f"{key}_ids.csv", index=False)
--- datasets/Smid_2020/compose.py
+++ datasets/Smid_2020/compose.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+key = "Smid_2020"
+
 df = pd.read_csv("https://osf.io/download/gxams/", usecols=['doi', 'included'])
 
 # adjust columns
@@ -9,9 +11,9 @@ df["doi"] = "https://doi.org/" + df["doi"].str.extract(r"(10.\S+)")
 df.rename({'included': 'label_included'}, axis=1, inplace=True)
 
 # export
-df.to_csv("Smid_2020_raw.csv", index=False)
+df.to_csv(f"{key}_raw.csv", index=False)
 
 df_new = df[["doi", "label_included"]].copy()
 df_new["openalex_id"] = None
 
-df_new[["doi", "openalex_id", "label_included"]].to_csv("Smid_2020_ids.csv", index=False)
+df_new[["doi", "openalex_id", "label_included"]].to_csv(f"{key}_ids.csv", index=False)
