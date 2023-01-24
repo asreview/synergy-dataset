e18d6a81540bf04db7e1dcb4a9f91f84288e76c9
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-01-24 10:30:19 +0100

    Add Wolters dataset in new format

---
+++ datasets/Wolters_2018/compose.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from asreview import ASReviewData
+
+key = "Wolter_2018"
+
+# load RIS into ASReviewData object
+asr_inclusions = ASReviewData.from_file("https://osf.io/a26sz/download")
+asr_search = ASReviewData.from_file("https://osf.io/hy8qe/download")
+
+# set labels and turn into single dataframe
+asr_inclusions.df['label_included'] = 1
+asr_search.df['label_included'] = 0
+df = pd.concat([asr_inclusions.df, asr_search.df], ignore_index=True)
+df.drop_duplicates(inplace=True)
+
+# adjust columns and drop missing and duplicate ids
+df['doi'] = "https://doi.org/" + df['url'].str.extract(r"(10.\S+)")
+# df["doi"] = None
+
+# save results to file
+df.to_csv(f"{key}_raw.csv", index=False)
+
+df_new = df[["doi", "label_included"]].copy()
+df_new["openalex_id"] = None
+
+df_new[["doi", "openalex_id", "label_included"]].to_csv(f"{key}_ids.csv", index=False)
