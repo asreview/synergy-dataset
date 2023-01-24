d6035cce3ea64a9fe6050ee540ada82702c488af
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-01-24 15:02:49 +0100

    Update release script

--- scripts/release.py
+++ scripts/release.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from pathlib import Path
 import math
+import argparse
 
 import json
 import tomli
 
 from pyalex import Works
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
+
+PAGE_SIZE = 50
 
 
 def main(key):
@@ -24,33 +27,33 @@ def main(key):
 
     print("Number of records after deduplication", len(result))
 
-    Path("release", key).mkdir(parents=True, exist_ok=True)
-    result.to_csv(Path("release", key, f"{key}.csv"), index=False)
+    Path("..", "odss-release", key).mkdir(parents=True, exist_ok=True)
+    result.to_csv(Path("..", "odss-release", key, f"{key}.csv"), index=False)
 
     # create zip
-    with ZipFile(Path("release", key, f"works.zip"), 'w') as zip_obj:
+    with ZipFile(Path("..", "odss-release", key, f"works.zip"), 'w', ZIP_DEFLATED) as zip_obj:
 
         x = 0
         while x < len(result):
 
-            works = Works()[result["openalex_id"].iloc[x:x+25].tolist()]
+            works = Works()[result["openalex_id"].iloc[x:x+PAGE_SIZE].tolist()]
 
-            r = (x, min(x+25, len(result)))
+            r = (x, min(x+PAGE_SIZE, len(result)))
 
             zip_obj.writestr(f"works_{r[0]}_{r[1]}.json", json.dumps(works))
-            # with open(Path("release", key, f"works_{r[0]}_{r[1]}.json"), "w") as f:
+
+            # with open(Path("..", "odss-release", key, f"works_{r[0]}_{r[1]}.json"), "w") as f:
             #     json.dump(works, f)
 
-            x += 25
+            x += PAGE_SIZE
 
-if __name__ == '__main__':
-    with open("datasets.toml", mode="rb") as fp:
-        config = tomli.load(fp)
+
+def render_metadata(config):
 
     for dataset in config["datasets"]:
         print(dataset["key"])
 
-        w = Works()[dataset["publication"]["openalex"]]
+        w = Works()["doi:" + dataset["publication"]["doi"]]
 
         print(w["doi"])
         print(w["title"])
@@ -59,21 +62,34 @@ if __name__ == '__main__':
         print(w["host_venue"]["display_name"])
         print(w["publication_year"])
 
-  # "Hall_2012": {
-  #   "link": "http://doi.org/10.5281/zenodo.1162952",
-  #   "license": "CC-BY Attribution 4.0 International",
+        # "Hall_2012": {
+        #   "link": "http://doi.org/10.5281/zenodo.1162952",
+        #   "license": "CC-BY Attribution 4.0 International",
 
-  #   "topic": "Software Fault Prediction",
-  #   "final_inclusions": true,
-  #   "title_abstract_inclusions": false,
-  # },
+        #   "topic": "Software Fault Prediction",
+        #   "final_inclusions": true,
+        #   "title_abstract_inclusions": false,
+        # },
+
+    return
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog = 'Build release metadata',
+        description = 'Lookup metadata via OpenAlex')
+
+    parser.add_argument('dataset_name')
+    args = parser.parse_args()
 
 
-        # if dataset["key"] != "hall_2012":
-        #     main(dataset["key"])
 
-        if dataset["key"] == "Meijboom_2022":
-            main(dataset["key"])
+    with open("datasets.toml", mode="rb") as fp:
+        config = tomli.load(fp)
 
+    # render_metadata()
 
+    for dataset in config["datasets"]:
 
+        if dataset["key"] == args.dataset_name:
+            main(args.dataset_name)
