368d706cf7ec610458e20acf805d9d02d9d61f41
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-01-28 23:53:19 +0100

    Update release script

--- scripts/release.py
+++ scripts/release.py
@@ -5,6 +5,7 @@ import argparse
 
 import json
 import tomli
+import requests
 
 from pyalex import Works
 from zipfile import ZipFile, ZIP_DEFLATED
@@ -12,9 +13,10 @@ from zipfile import ZipFile, ZIP_DEFLATED
 PAGE_SIZE = 50
 
 
-def main(key):
 
-    df = pd.read_csv(Path("datasets", key, f"{key}_ids.csv"))
+def package(input_fp, output_folder):
+
+    df = pd.read_csv(input_fp)
 
     print("Number of records in the list (before dedup)", len(df))
     print("Number of records with openalex_id", df["openalex_id"].notnull().sum())
@@ -26,15 +28,16 @@ def main(key):
         .sort_index()
     )
 
+    if len(result) == 0:
+        raise ValueError("No records in dataset after deduplication. Check dataset.")
+
     print("Number of records after deduplication", len(result))
 
-    Path("..", "odss-release", key).mkdir(parents=True, exist_ok=True)
-    result.to_csv(Path("..", "odss-release", key, f"{key}.csv"), index=False)
+    Path(output_folder).mkdir(parents=True, exist_ok=True)
+    result.to_csv(Path(output_folder, "ids.csv"), index=False)
 
     # create zip
-    with ZipFile(
-        Path("..", "odss-release", key, f"works.zip"), "w", ZIP_DEFLATED
-    ) as zip_obj:
+    with ZipFile(Path(output_folder, f"works.zip"), "w", ZIP_DEFLATED) as zip_obj:
 
         x = 0
         while x < len(result):
@@ -45,36 +48,47 @@ def main(key):
 
             zip_obj.writestr(f"works_{r[0]}_{r[1]}.json", json.dumps(works))
 
-            # with open(Path("..", "odss-release", key, f"works_{r[0]}_{r[1]}.json"), "w") as f:
-            #     json.dump(works, f)
-
             x += PAGE_SIZE
 
 
 def render_metadata(config):
 
-    for dataset in config["datasets"]:
-        print(dataset["key"])
+    metadata = config
+    metadata_works = []
 
-        w = Works()["doi:" + dataset["publication"]["doi"]]
+    for i, dataset in enumerate(config["datasets"]):
 
-        print(w["doi"])
-        print(w["title"])
-        # print(w["authorships"])
-        print([x["author"]["display_name"] for x in w["authorships"]])
-        print(w["host_venue"]["display_name"])
-        print(w["publication_year"])
+        try:
+            del metadata["datasets"][i]["scripts"]
+        except KeyError:
+            pass
 
-        # "Hall_2012": {
+        try:
+            print("Metadata of", dataset["key"])
+
+            w = Works()["doi:" + dataset["publication"]["doi"]]
+            metadata_works.append(dict(w))
+
+            metadata["datasets"][i]["publication"] = {
+                "openalex_id": w["id"],
+                "doi": w["doi"],
+                "title": w["title"],
+                "authors": "; ".join([x["author"]["display_name"] for x in w["authorships"]]),
+                "host_venue": w["host_venue"]["display_name"],
+                "publication_year": w["publication_year"]
+            }
+
+        except requests.exceptions.HTTPError as err:
+            print("ERROR with metadata of {}:".format(dataset["key"]), err)
+
         #   "link": "http://doi.org/10.5281/zenodo.1162952",
         #   "license": "CC-BY Attribution 4.0 International",
 
         #   "topic": "Software Fault Prediction",
-        #   "final_inclusions": true,
-        #   "title_abstract_inclusions": false,
-        # },
 
-    return
+    # print(metadata_works)
+
+    return metadata, metadata_works
 
 
 if __name__ == "__main__":
@@ -83,15 +97,29 @@ if __name__ == "__main__":
         prog="Build release metadata", description="Lookup metadata via OpenAlex"
     )
 
-    parser.add_argument("dataset_name")
+    parser.add_argument("dataset_name", default=None)
+    # parser.add_argument("--meta")
     args = parser.parse_args()
 
-    with open("datasets.toml", mode="rb") as fp:
+    with open("datasets.toml", "rb") as fp:
         config = tomli.load(fp)
 
-    # render_metadata()
+    if 1:
+        meta, works = render_metadata(config)
+
+        with open(Path("..", "odss-release", f"metadata.json"), "w") as f:
+            json.dump(meta, f, indent=2)
+
+        with open(Path("..", "odss-release", f"metadata_works.json"), "w") as f:
+            json.dump(works, f)
 
     for dataset in config["datasets"]:
 
         if dataset["key"] == args.dataset_name:
-            main(args.dataset_name)
+            package(
+                Path("datasets", args.dataset_name, f"{args.dataset_name}_ids.csv"),
+                Path("..", "odss-release", "datasets", args.dataset_name)
+            )
+            break
+    else:
+        raise ValueError(f"'{args.dataset_name}' not found.")
