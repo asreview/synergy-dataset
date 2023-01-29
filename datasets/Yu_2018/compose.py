b8e69a89b194e135a6f54db16718f905ba447eca
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-01-29 21:50:10 +0100

    Improve results Yu with another API

--- datasets/Yu_2018/compose.py
+++ datasets/Yu_2018/compose.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import argparse
 import re
+import pyalex
+import os
 
 import logging
 
@@ -10,12 +12,23 @@ logging.basicConfig(level=logging.INFO)
 
 EMAIL_POLITE_POOL = "asreview@uu.nl"
 
+API_KEY = os.getenv('IEEE_KEY')
 
 def compare_ieee(x, y):
 
     x_d = str(re.search(r'arnumber\=(\d+)', x).group(1))
 
     return x_d in y
+
+
+def get_ieee_record(x):
+
+    x_d = str(re.search(r'arnumber\=(\d+)', x).group(1))
+
+    r = requests.get(f"https://ieeexploreapi.ieee.org/api/v1/search/articles?article_number={x_d}&apikey={API_KEY}")
+    w = pyalex.Works()["https://doi.org/" + r.json()["articles"][0]["doi"]]
+
+    return (w["doi"], w["id"])
 
 
 def get_doi(row):
@@ -54,10 +67,15 @@ def get_doi(row):
                         logging.info("\tMatch")
                         return (r["doi"], r["id"])
                 else:
-                    logging.info("\tNo DOI for OpenAlex record.")
+                    logging.debug("\tNo DOI for OpenAlex record.")
+            else:
+                d, oa = get_ieee_record(row["PDF Link"])
+                return (d, oa)
 
         elif res_openalex["meta"]["count"] == 0:
             logging.info("No result found on OpenAlex: " + row['Document Title'])
+            d, oa = get_ieee_record(row["PDF Link"])
+            return (d, oa)
         else:
             w = res.json()['results'][0]
             logging.info("Result found {}".format(w["doi"]))
@@ -72,11 +90,16 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.subset == "Hall_2012":
-        df = pd.read_csv("https://zenodo.org/record/1162952/files/Hall.csv")
+        # df = pd.read_csv("https://zenodo.org/record/1162952/files/Hall.csv")
+        # df.to_csv(f"{args.subset}_raw.csv", index=False)
+        df = pd.read_csv(f"unlabeled_raw.csv", index=0).head(100)
+
         # df = pd.read_csv("https://zenodo.org/record/1162952/files/Wahono.csv")
         # df = pd.read_csv("https://zenodo.org/record/1162952/files/Radjenovic.csv", encoding="iso-8859-1")
         # ZipFile(BytesIO(urlopen("https://zenodo.org/record/1162952/files/Kitchenham.zip").read())
 
     df[["doi", "openalex_id"]] = df.apply(get_doi, axis=1, result_type="expand")
     df["label_included"] = np.where(df["label"] == "yes", 1, 0)
-    df[["doi", "openalex_id", "label_included"]].to_csv(f"{args.subset}_ids.csv", index=False)
+    print(df[["doi", "openalex_id", "label_included"]])
+    # df[["doi", "openalex_id", "label_included"]].to_csv(f"{args.subset}_ids.csv", index=False)
+    df[["doi", "openalex_id", "label_included"]].to_csv(f"unlabeled_ids.csv", index=True)
