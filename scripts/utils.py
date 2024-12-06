549b52609ffaba263dc9b032c58624e6cdc50d2e
Emily Westerbeek <emilywesterbeek23@gmail.com>
2024-12-06 12:13:06 +0100

    add extract_data function

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,7 @@
 """This script contains functionality to simplify compose scripts for new datasets"""
 
 import pandas as pd
+import numpy as np
 
 # All ID's we use to search in OpenAlex
 ID_SET = {"doi", "pmid", "title", "year"}
@@ -48,3 +49,50 @@ def write_ids_files(key: str, df: pd.DataFrame):
 
     df.to_csv(f"{key}_raw.csv", index=False)
     df.reindex(columns=OUTPUT_ID_SET).to_csv(f"{key}_ids.csv", index=False)
+
+
+def extract_data(df: pd.DataFrame, doi: str = "", pmid: str = "", title: str="", year: str=""):
+    """
+    Takes column names as input for where to find data.
+    For each row of the identifier that is empty: copy the data in there.
+    May apply a regex selection to get data in the right format.
+    """
+
+    # Add missing ID columns
+    for id in ID_SET.difference(list(df)):
+        df[id] = None
+
+    if doi:
+        df['doi'] = np.where(df['doi'].isnull(), extract_doi(df[doi]), df['doi'])
+
+    if pmid:
+        df['pmid'] = np.where(df['pmid'].isnull(), extract_pmid(df[pmid]), df['pmid'])
+
+    if title:
+        df['title'] = np.where(df['title'].isnull(), df[title], df['title'])
+
+    if year:
+        df['year'] = np.where(df['year'].isnull(), extract_year(df[year]), df['year'])
+
+    return df
+
+
+def extract_doi(s: str):
+    """Extracts doi from a string using regex, to be implemented"""
+
+    return s
+
+
+def extract_pmid(s: str):
+    """Extracts pmid from a string using regex, to be implemented"""
+
+    return s
+
+
+def extract_year(x):
+    """Extracts year from a string using regex"""
+
+    if isinstance(x, str):
+        return x.extract(r"(\d+)")
+
+    return x 
