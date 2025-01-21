d843032c6b9876fa67037306e5ab5f71c60c12ee
Emily Westerbeek <emilywesterbeek23@gmail.com>
2025-01-21 11:47:28 +0100

    Add corrected version of Moran

---
+++ datasets/Moran_2021_corrected/compose.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+key = "Moran_2021"
+
+df_labels = pd.read_excel("https://osf.io/gmvxy/download")
+df_abstracts = pd.read_excel("https://osf.io/5zw46/download")
+
+df_labels["title"] = df_labels["Title"]
+df_labels["label_included"] = (df_labels["Final Decision"] == "Include").astype(int)
+df_labels["label_abstract_included"] = 1
+df_labels = df_labels.reindex(columns=["title", "label_included", "label_abstract_included"])
+
+df = pd.merge(df_abstracts, df_labels, how="outer", on="title")
+
+df["doi"] = "https://doi.org/" + df["url"].str.extract(r"(10\.[^&]*)")[0]
+df['label_included'] = df['label_included'].fillna(0)
+df['label_abstract_included'] = df['label_abstract_included'].fillna(0)
+
+# save results to file
+df.to_csv(f"{key}_raw.csv", index=False)
+df.reindex(columns=["openalex_id", "doi", "label_included", "label_abstract_included", "method"]).to_csv(f"{key}_ids.csv", index=False)
