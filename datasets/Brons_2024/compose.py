b1520c52db79b4783fb0136f4dae7eb80f7f3191
EmilyWes <emilywesterbeek23@gmail.com>
2025-02-24 10:31:28 +0100

    update compose to new osf

--- datasets/Brons_2024/compose.py
+++ datasets/Brons_2024/compose.py
@@ -5,10 +5,12 @@ sys.path.append("../../scripts")
 import utils
 
 # Load the data
-df = pd.read_excel("https://osf.io/7pmrw/download")
+# df = pd.read_excel("https://osf.io/9am46/download")
+df = pd.read_excel("data/Data ASReview.xlsx")
 
 # Process data
 df.rename(columns={"Title": "title"}, inplace=True)
+df = utils.extract_doi(df, "Doi")
 df = utils.extract_labels(df, ft_col="Reason", ft_value="Included")
 df["label_abstract_included"] = 0
 df.loc[df["Reason"] != "Abstract", "label_abstract_included"] = 1
