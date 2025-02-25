d10d95fb1e4b3633d8a94fdbe0c44ba1bb91f87a
Emily Westerbeek <emilywesterbeek23@gmail.com>
2025-02-25 15:24:00 +0100

    uncommented doi from url

--- datasets/Pijls_2017/compose.py
+++ datasets/Pijls_2017/compose.py
@@ -8,7 +8,7 @@ import utils
 df = pd.read_excel("https://osf.io/download/krngx")
 
 df = utils.extract_doi(df, "doi", "", " \[doi\]", True)
-# df = utils.extract_doi(df, "url")
+df = utils.extract_doi(df, "url")
 df = utils.extract_pmid(df, "url", "pubmed\/")
 df = utils.rename_columns(df, ft_label="included")
 df = utils.drop_duplicates(df)
