a4abe141a4b060d13302c9086c845c7f413ccae7
Emily Westerbeek <emilywesterbeek23@gmail.com>
2025-04-22 17:15:32 +0200

    add dataset morteza

---
+++ datasets/Morteza_2023/compose.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sys
+
+sys.path.append("../../scripts")
+import utils
+
+# Read input
+sheets = pd.read_excel(
+    "https://zenodo.org/records/7533578/files/smell_datasets_slr__article_selection_R1_v3.xlsx?download=1",
+    engine="openpyxl",
+    sheet_name=None,
+)
+
+search = sheets["All"]
+tiab = sheets["exclusion_passed"]
+ft = sheets["inclusion-passed"]
+
+search = utils.rename_columns(search, title="Title")
+tiab = utils.rename_columns(tiab, title="Title")
+ft = utils.rename_columns(ft, title="Titles")
+
+df = utils.combine_datafiles(search, ft, tiab)
+df = utils.drop_duplicates(df)
+
+# Write output
+utils.write_ids_files("Morteza_2023", df)
