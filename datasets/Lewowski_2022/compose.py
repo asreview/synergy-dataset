27ae72bc13801ba5893588c2526eaabe5fe51f87
Emily Westerbeek <emilywesterbeek23@gmail.com>
2025-06-17 16:13:30 +0200

    add lewowski

---
+++ datasets/Lewowski_2022/compose.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sys
+
+sys.path.append("../../scripts")
+import utils
+
+df = pd.read_csv(
+    "https://zenodo.org/records/5647093/files/Initial_Screening.csv?download=1", sep=";", encoding='latin-1'
+)
+
+df = utils.extract_doi(df, "DOI")
+df = utils.rename_columns(df, title="Title")
+df = utils.extract_labels(df, "Final result", "Accepted", "include decision", "Y")
+
+df = utils.drop_duplicates(df)
+
+# Write output
+utils.write_ids_files("Lewowski_2022", df)
