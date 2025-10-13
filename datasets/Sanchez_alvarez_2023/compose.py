c3fafb1b5c9290a7c69c22d01c5736fa26c45407
EmilyWes <emilywesterbeek23@gmail.com>
2025-10-13 10:58:12 +0200

    add dataset sanchez_alvarez

---
+++ datasets/Sanchez_alvarez_2023/compose.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import sys
+
+sys.path.append("../../scripts")
+import utils
+
+# Read input
+df = pd.read_excel(
+    "https://osf.io/download/t2w6h/"
+)
+
+df = utils.rename_columns(df, year="Published Year")
+df = utils.extract_doi(df, "DOI")
+df = df.sort_values(by=['label_included'], ascending=False)
+df = utils.drop_duplicates(df)
+
+# Write output
+utils.write_ids_files("Sanchez_alvarez_2023", df)
