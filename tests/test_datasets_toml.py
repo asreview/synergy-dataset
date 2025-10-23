5d61beb69bffd71b0d1839c64ff68f0ee0c34982
Timo van der Kuil <5330531+timovdk@users.noreply.github.com>
2025-10-23 16:46:58 +0200

    Add Farisogullari_2023 and Improve_todo to skip_checks (#261)

--- tests/test_datasets_toml.py
+++ tests/test_datasets_toml.py
@@ -15,7 +15,7 @@ def test_datasets_toml():
 
 def test_dataset_keys():
 
-    skip_checks = ["Giesen_2021"]
+    skip_checks = ["Giesen_2021", "Farisogullari_2023", "Improve_todo"]
 
     with open("datasets.toml") as f:
         datasets = tomllib.loads(f.read())
