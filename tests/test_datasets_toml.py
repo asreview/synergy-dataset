e0574935bf768149f6874071641434564a7d1e9d
timovdk <5330531+timovdk@users.noreply.github.com>
2025-11-19 12:33:28 +0100

    Refactor test_dataset_keys for clarity and remove debug print statement

--- tests/test_datasets_toml.py
+++ tests/test_datasets_toml.py
@@ -4,7 +4,6 @@ import unicodedata
 
 
 def test_datasets_toml():
-
     with open("datasets.toml") as f:
         data = f.read()
 
@@ -20,14 +19,13 @@ def test_dataset_keys():
         datasets = tomllib.loads(f.read())
 
     for dataset in datasets["datasets"]:
-        print(dataset["key"], flush=True)
         if dataset["key"] in skip_checks:
             continue
         if dataset["key"] == "Farisogullari_2023":
             dataset["key"] = "Farisogulları_2023"
         if dataset["key"] == "Giesen_2021":
             dataset["key"] = "Bakker-Jacobs_2021"
-        
+
         if dataset["key"] == "Aouad_2024":
             work = pyalex.Works()["https://openalex.org/W4396814222"]
         elif dataset["key"] == "Hilfiker_2017":
@@ -36,9 +34,16 @@ def test_dataset_keys():
             work = pyalex.Works()["https://doi.org/" + dataset["publication"]["doi"]]
 
         surname = work["authorships"][0]["author"]["display_name"].split(" ")[-1]
-        surname = surname.replace("‐", "-").replace("–", "-").replace("—", "-").replace("−", "-")
-        surname = ''.join(
-            c for c in unicodedata.normalize('NFD', surname) if unicodedata.category(c) != 'Mn'
+        surname = (
+            surname.replace("‐", "-")
+            .replace("–", "-")
+            .replace("—", "-")
+            .replace("−", "-")
+        )
+        surname = "".join(
+            c
+            for c in unicodedata.normalize("NFD", surname)
+            if unicodedata.category(c) != "Mn"
         )
 
         openalex_key = f"{surname}_{work['publication_year']}"
