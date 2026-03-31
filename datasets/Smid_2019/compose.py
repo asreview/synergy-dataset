470f6d5bd82eb8313f517e7b230154563f50ea03
Timo van der Kuil <5330531+timovdk@users.noreply.github.com>
2026-03-31 10:36:51 +0200

    Smid_2019: Fix inclusion (#311)

--- datasets/Smid_2019/compose.py
+++ datasets/Smid_2019/compose.py
@@ -14,5 +14,12 @@ df = utils.extract_pmid(df, "url", "http://www.ncbi.nlm.nih.gov/pmc/articles/PMC
 df = utils.rename_columns(df, ft_label="included", authors="author")
 df = utils.drop_duplicates(df)
 
+# We remove this doi, since it does not exist in the original data or paper
+df = df[~df["doi"].astype(str).str.contains("10.3758/s13428-017-0870-1", na=False)]
+
+# We add this oa_id/doi/pmid, since it is an inclusion in the original paper
+ft_manual = pd.DataFrame([{"openalex_id": "https://openalex.org/w2609159880", "doi": "https://doi.org/10.1080/10705511.2017.1312407", "pmid": 29662296, "label_included": 1}])
+df = pd.concat([df, ft_manual])
+
 # Write output
 utils.write_ids_files("Smid_2019", df)
