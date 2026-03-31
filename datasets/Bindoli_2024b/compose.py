349268569a58d66735f8b8f788e465b028184225
Timo van der Kuil <5330531+timovdk@users.noreply.github.com>
2026-03-31 10:28:08 +0200

    Bindoli_2024b: Replace poster with article (#307)

--- datasets/Bindoli_2024b/compose.py
+++ datasets/Bindoli_2024b/compose.py
@@ -14,5 +14,19 @@ df = utils.extract_doi(df, "doi", "", "", True)
 df = df.sort_values(by=['label_included'], ascending=False)
 df = utils.drop_duplicates(df)
 
+
+# Insert openalex_id for the specific DOI and replace DOI with the correct one (poster vs article that was actually cited)
+df["openalex_id"] = df["openalex_id"].astype("string")
+mask = df["doi"].str.contains(
+    "10.1136/annrheumdis-2022-eular.803", na=False
+)
+
+df.loc[mask, ["doi", "openalex_id"]] = [
+    "https://doi.org/10.1136/ard-2022-223739",
+    "https://openalex.org/W4362460104"
+]
+
+
+
 # Write output
 utils.write_ids_files("Bindoli_2024b", df)
