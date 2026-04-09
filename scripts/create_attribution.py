e0f6c41420d67c34ef621bf934483f4a722e8a1a
Emily Westerbeek <emilywesterbeek23@gmail.com>
2026-04-09 16:01:24 +0200

    updated attribution

--- scripts/create_attribution.py
+++ scripts/create_attribution.py
@@ -2,26 +2,64 @@
 
 import tomli
 from pathlib import Path
+import json
+import pandas as pd
 
 
 authors = []
-citations = []
+citations = ""
 collection_citations = []
 
 with open("datasets.toml", "rb") as fp:
-		config = tomli.load(fp)
+	config = tomli.load(fp)
 
 	for dataset in config["datasets"]:
 		if "active" in dataset and not dataset["active"]:
 			print(f"Not active {dataset['key']}")
 			continue
 
-		dataset_path = Path("..", "synergy-release", dataset["key"])
+		dataset_path = Path("..", "synergy-release-abstracts", dataset["key"])
 
 		print(f"Processing dataset {dataset['key']}")
 
-		with open(Path(dataset_path, "CITATION.txt"), "r") as f:
-			citation = f.read()
-			citations.add(citation)
+		with open(Path(dataset_path, "CITATION.txt"), "r", encoding="utf-8") as f:
+			citations = citations + f" > [{dataset['key']}] " + f.read() + "\n"
 
-print(citations)
+		# collections : todo
+		collection_path = Path(dataset_path, "CITATION_collection.txt")
+		if Path.is_file(collection_path):
+			with open(collection_path, "r", encoding="utf-8") as f:
+				collection_citations.append(f.read())
+
+		# authors:
+		with open(Path(dataset_path, "metadata_publication.json"), "r", encoding="utf-8") as f:
+			data = json.load(f)
+			for authorship in data["authorships"]:
+				name = authorship["author"]["display_name"]
+				last_name = name.split()[-1]
+				orcid = authorship["author"]["orcid"]
+				author_data = name
+				if orcid:
+					author_data = f"[{name}]({orcid})"
+				authors.append(last_name + "|" + author_data)
+
+# Dedup, Sort and Remove last name
+authors = list(set(authors))
+authors.sort()
+authors = [a.split("|")[1] for a in authors]
+
+# Dedup
+collection_citations = list(set(collection_citations))
+collections = ""
+for c in collection_citations:
+	collections = collections + f" > {c}\n"
+
+
+
+with open("attribution_auto.md", "w", encoding="utf-8") as f:
+	f.write("# Attribution\n\nWe would like to thank the following authors for openly sharing the data corresponding to their systematic review:\n\n")
+	f.write(", ".join(authors))
+	f.write("\n\n\nReferences to datasets:\n\n")
+	f.write(citations)
+	f.write("\n\n\nWe thank the authors of the following collections of systematic reviews:\n\n")
+	f.write(collections)
