9e5f17df549ebf30bc74cd400b9a1b99d8fc938f
Emily Westerbeek <emilywesterbeek23@gmail.com>
2026-05-19 13:04:39 +0200

    corrected errornous input data

--- datasets/Butink_2023/compose.py
+++ datasets/Butink_2023/compose.py
@@ -10,6 +10,10 @@ df = pd.read_excel("https://osf.io/download/fr4u7/")
 df = utils.rename_columns(df, year="Year", ti_ab_label="OPTIONALLY\nIncluded after the title/abstract phase = 1, \nExcluded after the title/abstract phase = 0")
 df = utils.extract_doi(df, "doi", "", "", True)
 df = df.sort_values(by=['label_included'], ascending=False)
+
+# Correct an errornous label in the input data
+df.at[2533, 'label_abstract_included'] = 1
+
 df = utils.drop_duplicates(df)
 
 # Write output
