1c944bf32a448c9e3546475f20fdf213b5f7a73e
timovdk <5330531+timovdk@users.noreply.github.com>
2026-08-26 11:21:46 +0200

    synergy_plus_2.1

--- scripts/run_compose.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem "$PSScriptRoot\..\datasets" -Directory | ForEach-Object {
-    Push-Location $_.FullName
-    python compose.py
-    Pop-Location
-}
--- scripts/run_compose.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-for dir in ../datasets/*/; do
-    ( cd "$dir" && python compose.py )
-done
